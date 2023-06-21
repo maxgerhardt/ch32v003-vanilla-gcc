c2bc0930b680815e0227a545d7a88a1ff4be56ef
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2023-06-21 13:32:48 +0200

    Correct elf name for disassembly dump

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ rem Build and Disassemble for RV32EC
 "%COMPILER_PATH%\riscv-none-elf-gcc" -o firmware.elf -march=rv32ec -mabi=ilp32e -Os -msave-restore -nostartfiles -T Link_CH32V00x.ld -I. main_application.c %RISC_CORE_FILE% %SDK_FILES% %DEBUG_FILE% %SYSTEM_FILE% %STARTUP_FILE%
 
 "%COMPILER_PATH%\riscv-none-elf-objcopy" -Obinary firmware.elf firmware.bin
-"%COMPILER_PATH%\riscv-none-elf-objdump" -d firmware_rv32ec.elf > firmware.S
+"%COMPILER_PATH%\riscv-none-elf-objdump" -d firmware.elf > firmware.S
 
 rem Flash using WCH's OpenOCD 
 set OPENOCD_PATH=C:\MounRiver\MounRiver_Studio\toolchain\OpenOCD\bin
